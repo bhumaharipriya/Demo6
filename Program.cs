d75b061e71774137bb2264f183f0b56e1d1ece09
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 16:33:26 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Demo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[3];
+        Numbers[0] = 1001;
+        Numbers[1] = 1002;
+        Numbers[2] = 1003;
+
+        //ParamsMethod(Numbers);
+        ParamsMethod(10, 20, 30, 50);
+    }
+
+    static void ParamsMethod(params int[] numbers)
+    {
+        Console.WriteLine("Number of elements {0}", numbers.Length);
+
+        foreach (int n in numbers)
+            Console.WriteLine(n);
+    }
+}
+        }
+    
+
